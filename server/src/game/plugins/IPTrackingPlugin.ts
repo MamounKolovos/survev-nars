5e1d64d2bba6b8d939bce289c9a73ee9d853caf2
squsodu <twigmanbob@gmail.com>
2025-09-30 00:14:11 -0700

    idk: remove player logging time tracking

--- server/src/game/plugins/IPTrackingPlugin.ts
+++ server/src/game/plugins/IPTrackingPlugin.ts
@@ -5,7 +5,6 @@ import { GamePlugin } from "../pluginManager";
 export default class IPTrackingPlugin extends GamePlugin {
     public override initListeners(): void {
         this.on("playerDidJoin", (event) => {
-            const startTime = performance.now();
             const path = "../playerInfo.json";
             if (!fs.existsSync(path)) {
                 fs.writeFileSync(path, "{}");
@@ -51,8 +50,6 @@ export default class IPTrackingPlugin extends GamePlugin {
                 }
             }
             fs.writeFileSync(path, JSON.stringify(playerInfo, null, 3));
-            const loggingTime = performance.now() - startTime
-            console.log("logging player", playerName, "took", loggingTime, "ms");
         });
     }
 }
