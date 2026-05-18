76531f36b3be516005969a1eb3b9027410cf6bcb
chpsterz <paaxyman@gmail.com>
2026-05-17 23:13:56 -0400

    guess whos back, back again

--- shared/defs/gameObjects/throwableDefs.ts
+++ shared/defs/gameObjects/throwableDefs.ts
@@ -291,6 +291,72 @@ export const ThrowableDefs: Record<string, ThrowableDef> = {
             deploy: "frag_deploy_01",
         },
     },
+    poison_smoke: {
+        name: "Poison Smoke",
+        type: "throwable",
+        quality: 0,
+        explosionType: "explosion_smoke",
+        inventoryOrder: 3,
+        cookable: false,
+        explodeOnImpact: false,
+        playerCollision: false,
+        fuseTime: 1.5,
+        aimDistance: 0,
+        rad: 1,
+        throwPhysics: {
+            playerVelMult: 0.6,
+            velZ: 5,
+            speed: 15,
+            spinVel: 10 * Math.PI,
+            spinDrag: 1,
+        },
+        speed: { equip: 0, attack: 0 },
+        lootImg: {
+            sprite: "loot-throwable-smoke.img",
+            tint: 0xff00,
+            border: "loot-circle-outer-01.img",
+            borderTint: 0,
+            scale: 0.2,
+        },
+        worldImg: {
+            sprite: "proj-smoke-nopin-nolever.img",
+            scale: 0.12,
+            tint: 0xffffff,
+        },
+        handImg: {
+            equip: {
+                right: {
+                    sprite: "proj-smoke-pin.img",
+                    pos: { x: 3, y: 4.2 },
+                    scale: 0.14,
+                },
+                left: { sprite: "none" },
+            },
+            cook: {
+                right: {
+                    sprite: "proj-smoke-nopin.img",
+                    pos: { x: 3, y: 4.2 },
+                    scale: 0.14,
+                },
+                left: {
+                    sprite: "proj-frag-pin-part.img",
+                    pos: { x: 3, y: 4.2 },
+                    scale: 0.14,
+                },
+            },
+            throwing: {
+                right: { sprite: "none" },
+                left: { sprite: "none" },
+            },
+        },
+        useThrowParticles: true,
+        sound: {
+            pullPin: "frag_pin_01",
+            throwing: "frag_throw_01",
+            pickup: "frag_pickup_01",
+            deploy: "frag_deploy_01",
+        },
+    },
     smoke: {
         name: "Smoke Grenade",
         type: "throwable",
