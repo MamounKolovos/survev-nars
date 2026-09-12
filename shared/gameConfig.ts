f45340f5651db0509f1a601bc2f9e456eb568cc8
MamounKolovos <mamoun.kolovos@gmail.com>
2026-09-12 18:35:48 -0400

    chore: bump protocol

--- shared/gameConfig.ts
+++ shared/gameConfig.ts
@@ -112,8 +112,8 @@ export const GameConfig = {
     // the protocol we originated from was 78
     // remember to bump this every time a serialization function is changed
     // or a definition item added, removed or moved
-    protocolVersion: 1023,
-    replayVersion: 9,
+    protocolVersion: 1024,
+    replayVersion: 10,
     Input,
     EmoteSlot,
     WeaponSlot,
